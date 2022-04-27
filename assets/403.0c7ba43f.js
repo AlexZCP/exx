509e9f323602d029a43a531fd704f5bde1987d22
Alex Z <olesmx@gmail.com>
2022-04-27 18:42:00 +0300

    Added new version

---
+++ assets/403.0c7ba43f.js
@@ -0,0 +1,1 @@
+import{_ as s}from"./index.7bb75959.js";import{l as n,D as c,t as e}from"./naive-ui.871da52f.js";var t={};const o={},a={class:"about"},r=e("h1",null,"HTTP 403. Restricted.",-1),_=e("p",null,"You have not enugh rights",-1),i=[r,_];function d(f,l){return n(),c("div",a,i)}typeof t=="function"&&t(o);var p=s(o,[["render",d]]);export{p as default};
